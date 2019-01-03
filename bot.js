1f371ebd4ca041ba990e1141842954de5f68f6a9
iiHaaady <46338150+iiHaaady@users.noreply.github.com>
2019-01-03 11:18:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,8 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("530266479644508162")
 setInterval(function() {
-channel.send(`**! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.! - Haaăaadý.**`}, 30)
+channel.send(`**! - Haaăaadý. ! - Haaăaadý. ! - Haaăaadý. ! - Haaăaadý. ! - Haaăaadý. ! - Haaăaadý. ! - Haaăaadý. ! - Haaăaadý. ! - Haaăaadý. ! - Haaăaadý. ! - Haaăaadý. ! - Haaăaadý.! - Haaăaadý.**`);
+}, 30)
 })
 
 client.login(process.env.BOT_TOKEN);
